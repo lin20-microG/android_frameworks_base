GrapheneOS-Archive/platform_frameworks_base-old

add exec spawning sysprop to ExtSettings

core/java/android/ext/settings/ExtSettings.java
@@ -119,6 +119,9 @@ public class ExtSettings {
         }
     };
 
+    public static final BoolSysProperty EXEC_SPAWNING = new BoolSysProperty(
+            "persist.security.exec_spawn", true);
+
     private ExtSettings() {}
 
     // used for making settings defined in this class unreadable by third-party apps

lin20-microG/android_frameworks_base

 prop: persist.security.exec_spawn_new
Change-Id: I13fb5c7ec5f129f317a80c2ec568d3d55ff1aa18
Signed-off-by: MSe <mse1969@posteo.de>

core/java/android/ext/settings/ExtSettings.java
@@ -13,6 +13,9 @@ public class ExtSettings {
             // default value: 3 days
             (int) TimeUnit.DAYS.toMillis(3));
 
+    public static final BoolSysProperty EXEC_SPAWNING = new BoolSysProperty(
+            "persist.security.exec_spawn_new", true);
+
     private ExtSettings() {}
 
     // used for making settings defined in this class unreadable by third-party apps