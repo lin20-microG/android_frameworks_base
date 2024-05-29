ce8caa9ffcb51b1ae0fbc299d20c0cfd5bd52931
mse1969 <mse1969@posteo.de>
2024-05-29 13:37:53 +0200

    Truncate user data to a limit of 500 characters
    Fix vulnerability that allows creating users with no restrictions. This is done by creating an intent to create a user and putting extras that are too long to be serialized. It causes IOException and the restrictions are not written in the file.
    
    By truncating the string values when writing them to the file, we ensure that the exception does not happen and it can be recorded correctly.
    
    Bug: 293602317
    Test: install app provided in the bug, open app and click add. Check logcat to see there is no more IOException. Reboot the device by either opening User details page or running adb shell dumpsys user | grep -A12 heen and see that the restrictions are in place.
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:59042a32c7e192d160c295ecb6477a09bb5da0bb)
    Merged-In: I633dc10974a64ef2abd07e67ff2d209847129989
    Change-Id: I633dc10974a64ef2abd07e67ff2d209847129989

--- services/core/java/com/android/server/pm/UserManagerService.java
+++ services/core/java/com/android/server/pm/UserManagerService.java
@@ -258,6 +258,8 @@ public class UserManagerService extends IUserManager.Stub {
 
     private static final int USER_VERSION = 9;
 
+    private static final int MAX_USER_STRING_LENGTH = 500;
+
     private static final long EPOCH_PLUS_30_YEARS = 30L * 365 * 24 * 60 * 60 * 1000L; // ms
 
     static final int WRITE_USER_MSG = 1;
